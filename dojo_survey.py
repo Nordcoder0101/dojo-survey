68454ba15fdf64d9d7c53e47686b5c4a6bcc4314
Greg <greg@Gregs-MacBook-Air.local>
2018-11-02 07:29:05 -0700

    form submits info to new page, andcan return to index.html with button. No styling

--- dojo_survey.py
+++ dojo_survey.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
  
 app = Flask(__name__)
 
@@ -8,6 +8,21 @@ def index():
     
     return render_template('index.html')
 
+@app.route("/users", methods=['POST'])
+def submit_survey():
+    print("Got post info")
+    print(request.form)
+    name_from_form = request.form['name']
+    school_from_form = request.form['dojo']
+    language_from_form = request.form['favorite_language']
+    comments_from_form = request.form['comments']
+
+    return render_template('show.html', name_from_form = name_from_form, school_from_form=school_from_form, language_from_form=language_from_form, comments_from_form =comments_from_form)
+
+@app.route("/back_to_form")
+def back_to_form():
+  print('click')
+  return render_template('index.html')
 
 if __name__ =='__main__':
     app.run(debug=True)
